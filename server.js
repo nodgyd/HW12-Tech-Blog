376b5145e4a52e4796603ba4ec9b49a651647a7f
Naji Dia <najimdia@gmail.com>
2022-01-26 16:26:54 -0800

    fixed handlebars

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ const hbs = exphbs.create({
   }
 });
 
-app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+app.engine("handlebars", hbs.engine);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
